87d663d4969c79047fc52a2d8007b57a322c11c9
Hasnain Naqvi <hasnain.naqvi@kcl.ac.uk>
2023-09-29 00:55:20 +0100

    Finish and test primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n > 1:
+        #check for factors
+        for i in range(2,n):
+            if (n % i) == 0:
+                return False
+        return True
+            
 def primes(number_of_primes):
     list = []
+    
+    if number_of_primes <= 0:
+        raise ValueError("Enter a positive integer")
+    
+    n = 2
+    while len(list) < number_of_primes:
+        if is_prime(n):
+            list.append(n)
+        n += 1
+
     return list
